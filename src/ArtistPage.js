499305b723c7748038a1c85a1bfce923cb60ce75
Linh <llkim@tempo.io>
2020-09-19 10:04:26 -0400

    missing grid now but logos in and padding a little bit more responsive

--- src/ArtistPage.js
+++ src/ArtistPage.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
 import { I18nContext } from "./i18n";
+import QuebecLogo from "./components/Icons/QuebecLogo";
+import MontrealLogo from "./components/Icons/MontrealLogo";
 const TriniTrang = require("./images/TriniTrang.png");
 const TriniTrangCloseUp = require("./images/Trini_CloseUp.png");
 
@@ -8,43 +10,51 @@ const StyledSection = styled.section`
   display: flex;
   flex-wrap: wrap;
   height: 100%;
-  display: flex;
   align-items: center;
   justify-content: center;
   background: #000;
-  border-top: 1px solid #efece2;
-  border-bottom: 1px solid #efece2;
+  @media (max-width: 768px) {
+    flex-basis: column;
+    height: 1120px;
+  }
 `;
-const StyledCloseUpContainer = styled.div`
-  position: relative;
-  height: 898px;
-  width: 864px;
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-right: 1px solid #efece2;
-  border-left: 1px solid #efece2;
-  padding: 0px 84px;
-`;
-
 const MainPagePortrait = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100vh;
 `;
-const PortraitImage = styled.div`
+const StyledImage = styled.img`
   flex: 1 300px;
   min-width: 0;
   width: 50%;
-  height: 100%;
-  background-image: url(${TriniTrang});
-  background-size: cover;
-  background-repeat: no-repeat;
+  height: auto;
+`;
+const StyledCloseUpImage = styled.img`
+  flex: 1 300px;
+  min-width: 0;
+  width: 100%;
+  height: auto;
+
+  max-width: 704px;
+  max-height: 898px;
+  @media (max-width: 768px) {
+    order: 1;
+  }
+`;
+const StyledBuffer = styled.div`
+  min-width: 0;
+  flex-basis: 200px;
+  @media (max-width: 768px) {
+    flex-basis: auto;
+  }
 `;
 
-const MainPagePortraitTextContainer = styled.div`
+const RightPanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   flex: 1 300px;
   min-width: 0;
   height: 100%;
@@ -59,31 +69,50 @@ const MainDescriptionContainer = styled.div`
   display: flex;
   flex-direction: column;
   font-size: 16px;
-  line-height: 20px
+  line-height: 20px;
   text-align: center;
   padding: 0px 50px;
- 
+  color: #efece2;
+`;
+const StyledProjectHistory = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+  padding: 0px 50px;
+  color: #efece2;
 `;
 const MainPagePortraitText = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 80%;
   font-size: 16px;
   line-height: 20px;
   text-align: center;
-
+  @media (max-width: 768px) {
+    height: 100%;
+  }
 `;
 const MainInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
-  padding-bottom: 32px;
+  padding: 32px 0px;
   align-items: center;
+  @media (max-width: 768px) {
+    padding: 28px 0px;
+  }
 `;
 const SectionHeaderText = styled.div`
   font-size: 56px;
   line-height: 65px;
   text-align: center;
+  color: #efece2;
+  margin: 40px 12px;
+    @media (max-width: 768px) {
+    margin: 16px 12px;
+  }
 `;
 const SubheaderText = styled.div`
   font-size: 13px;
@@ -101,6 +130,10 @@ const StyledArtistText = styled.span`
   color: #efece2;
   text-transform: uppercase;
   margin-bottom: 24px;
+    @media (max-width: 768px) {
+    flex-basis: column;
+    margin-bottom: 16px;
+  }
 `;
 const StyledEssentielsText = styled.div`
   font-size: 56px;
@@ -116,20 +149,28 @@ const PhotoGridContainer = styled.div`
   height: 100%;
   width: 100%;
 `;
+const StyledHistoryFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  color: #efece2;
+  height: 80px;
+  width: 100%;
+  padding: 15px;
+`;
 
 const ArtistPage = () => {
   const { translate } = useContext(I18nContext);
   return (
     <MainPagePortrait>
       <StyledSection className="main-portrait">
-        <PortraitImage>
-          {/*<img src={TriniTrang} width={700} />*/}
-        </PortraitImage>
-        <MainPagePortraitTextContainer>
+        <StyledImage src={TriniTrang} />
+        <RightPanel>
           <MainPagePortraitText>
             <MainInfoContainer>
               <StyledArtistText>
-                {translate("portrait1.artistText")}
+                {translate("portrait1.biography")}
               </StyledArtistText>
               <SectionHeaderText>
                 {translate("portrait1.fullName")}
@@ -144,49 +185,43 @@ const ArtistPage = () => {
               {translate("portrait1.description")}
             </MainDescriptionContainer>
           </MainPagePortraitText>
-        </MainPagePortraitTextContainer>
+        </RightPanel>
       </StyledSection>
-      {/*<StyledSection className="close-up-portrait">*/}
-      {/*  <MainPagePortraitTextContainer>*/}
-      {/*    <StyledCloseUpContainer>*/}
-      {/*      <img src={TriniTrangCloseUp} />*/}
-      {/*    </StyledCloseUpContainer>*/}
-      {/*  </MainPagePortraitTextContainer>*/}
-      {/*</StyledSection>*/}
-      {/*<StyledSection className="les-essentiels">*/}
+      <StyledSection>
+        <StyledBuffer />
+        <StyledCloseUpImage src={TriniTrangCloseUp} />
+        <StyledBuffer />
+      </StyledSection>
+
+      {/*<StyledSection>*/}
       {/*  <StyledEssentielsText>*/}
       {/*    {translate("the-essentials")}*/}
       {/*  </StyledEssentielsText>*/}
 
-      {/*  <PhotoGridContainer>photos here</PhotoGridContainer>*/}
+      {/*/!*  <PhotoGridContainer>photos here</PhotoGridContainer>*!/*/}
       {/*</StyledSection>*/}
 
-      {/*<StyledSection className="about-the-project">*/}
-      {/*  <MainPagePortraitTextContainer>*/}
-      {/*    <SectionHeaderText>Le Project</SectionHeaderText>*/}
-      {/*    <MainDescriptionContainer>*/}
-      {/*      Lorem ipsum dolor sit amet id porta nibh venenatis cras sed felis*/}
-      {/*      eget velit aliquet sagittis id consectetur purus ut faucibus*/}
-      {/*      pulvinar elementum integer enim neque volutpat ac tincidunt vitae*/}
-      {/*      semper quis lectus nulla at volutpat diam ut venenatis tellus in*/}
-      {/*      metus vulputate eu scelerisque felis imperdiet proin fermentum leo*/}
-      {/*      vel orci porta non pulvinar neque laoreet suspendisse interdum*/}
-      {/*      consectetur libero id faucibus nisl tincidunt eget nullam non nisi*/}
-      {/*      est sit amet facilisis magna etiam tempor orci eu lobortis elementum*/}
-      {/*      nibh tellus molestie nunc non blandit massa enim nec dui nunc mattis*/}
-      {/*      enim ut tellus elementum sagittis vitae et leo duis ut diam quam*/}
-      {/*      nulla porttitor massa id neque aliquam vestibulum morbi blandit*/}
-      {/*      cursus risus at ultrices mi tempus imperdiet nulla malesuada*/}
-      {/*      pellentesque elit eget gravida cum sociis natoque penatibus et*/}
-      {/*      magnis dis parturient montes nascetur ridiculus mus mauris vita.*/}
-      {/*      Ultricies leo integer malesuada nunc vel risus commodo viverra*/}
-      {/*      maecenas accumsan lacus vel facilisis volutpat amet dictum sit amet*/}
-      {/*      justo donec enim diam vulputate ut pharetra sit amet aliquam id diam*/}
-      {/*      maecenas ultricies mi eget mauris pharetra et ultrices neque ornare*/}
-      {/*      aenean euismod.*/}
-      {/*    </MainDescriptionContainer>*/}
-      {/*  </MainPagePortraitTextContainer>*/}
-      {/*</StyledSection>*/}
+      <StyledSection className="about-the-project">
+        <StyledBuffer />
+        <SectionHeaderText>Le Project</SectionHeaderText>
+        <StyledProjectHistory>
+          Lorem ipsum dolor sit amet id porta nibh venenatis cras sed felis eget
+          velit aliquet sagittis id consectetur purus ut faucibus pulvinar
+          elementum integer enim neque volutpat ac tincidunt vitae semaliquam
+          vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet
+          nulla malesuada pellentesque elit eget gravida cum sociis natoque
+          penatibus et magnis dis parturient montes nascetur ridiculus mus
+          mauris vita. Ultricies leo integer malesuada nunc vel risus commodo
+          viverra maecenas accumsan lacus vel facilisis volutpat amet dictum sit
+          amet justo donec enim diam vulputate ut pharetra sit amet aliquam id
+          diam maecenas ultricies mi eget mauris pharetra et ultrices neque
+          ornare aenean euismod.
+        </StyledProjectHistory>
+        <StyledHistoryFooter>
+          <MontrealLogo />
+          <QuebecLogo />
+        </StyledHistoryFooter>
+      </StyledSection>
     </MainPagePortrait>
   );
 };
