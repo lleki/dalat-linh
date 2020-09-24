774489ea2e8cd5dcf80969da7a8e4545ed580cd5
Linh <llkim@tempo.io>
2020-09-24 16:04:45 -0400

    added more photo/profiles and uncommented from nav

--- src/PreviewPicture.js
+++ src/PreviewPicture.js
@@ -19,10 +19,10 @@ position:absolute;
 const artistImages = {
   1: "richard",
   2: "eli-tarek",
-  // 3: "jodie-ann",
+  3: "jodie-ann",
   // 4: "gabriella",
   5: "estelle",
-  // 6: "rowan",
+  6: "rowan",
   // 7: "yolande",
   // 8: "shekh",
   9: "laurence",
